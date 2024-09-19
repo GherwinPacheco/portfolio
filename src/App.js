e51e45c414ed64c022dd828e55cb2ce929f52f7b
GherwinPacheco <gherwinpacheco1@gmail.com>
2024-09-19 12:51:03 +0800

    added page routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import ScrollFade from './components/ScrollFade';
-import Tools from './components/Tools';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Blank } from 'pages/Blank.js';
+import { Home } from 'pages/Home.js';
 
 const App = () => {
 
-  const tools = [
-    {tool_name: 'HTML5', src: './img/html5.png'},
-    {tool_name: 'CSS', src: './img/css.png'},
-    {tool_name: 'Javascript', src: './img/js.png'},
-    {tool_name: 'PHP', src: './img/php.png'},
-    {tool_name: 'React', src: './img/react.png'}
-  ]
   return (
-    <div className="">
-
-      <div className=" bg-gray-700" style={{height: "800px"}}></div>
-      
-      <div className="flex items-center justify-center">
-        {tools.map((item, index) => (
-          <ScrollFade key={index} delay={index} direction="top">
-            <Tools {...item} />
-          </ScrollFade>
-        ))}
-      </div>
-      
-
-      
-
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Blank/> } />
+        <Route path="/home" element={ <Home/> } />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+export const Home = () => {
+    const tools = [
+        {tool_name: 'HTML5', src: './img/html5.png'},
+        {tool_name: 'CSS', src: './img/css.png'},
+        {tool_name: 'Javascript', src: './img/js.png'},
+        {tool_name: 'PHP', src: './img/php.png'},
+        {tool_name: 'React', src: './img/react.png'}
+    ];
+  return (
+    <div className="">
+
+      <div className=" bg-gray-700" style={{height: "800px"}}></div>
+      
+      <div className="flex items-center justify-center">
+        {tools.map((item, index) => (
+          <ScrollFade key={index} delay={index} direction="top">
+            <Tools {...item} />
+          </ScrollFade>
+        ))}
+      </div>
+      
+
+      
+
+      
+    </div>
+  );
+};
