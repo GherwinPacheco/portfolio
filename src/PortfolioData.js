dd5efa6b424df9fce126e665e15a3390525ce075
GherwinPacheco <gherwinpacheco1@gmail.com>
2024-11-07 13:00:45 +0800

    added links for projects

--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -55,19 +55,22 @@ export const Projects = [
         title: 'Inventory and Sales Management System', 
         tools: ['HTML5', 'CSS', 'Javascript', 'Bootstrap', 'PHP', 'MySQL'],
         images: ['inv_sales_1.png', 'inv_sales_2.png', 'inv_sales_3.png'],
-        description: 'Developed as my capstone project for Samson\'s Pharmacy, the Inventory and Sales Management System is a comprehensive solution designed for efficient tracking of inventory levels, monitoring product status, processing transactions, and generating reports.'
+        description: 'Developed as my capstone project for Samson\'s Pharmacy, the Inventory and Sales Management System is a comprehensive solution designed for efficient tracking of inventory levels, monitoring product status, processing transactions, and generating reports.',
+        link: 'https://github.com/GherwinPacheco/samson_pharmacy'
     },
     {
         title: 'Furniture Ordering System', 
         tools: ['HTML5', 'CSS', 'Javascript', 'Bootstrap', 'PHP', 'MySQL'],
         images: ['ordering_1.png', 'ordering_2.png', 'ordering_3.png', 'ordering_4.png'],
-        description: 'The Furniture Ordering System is a freelance project I developed that allows customers to browse and order furniture online, as well as submit requests for custom-made pieces. The system includes an admin panel where the shop owner can manage orders, customer records, and generate detailed sales and inventory reports for efficient business management.'
+        description: 'The Furniture Ordering System is a freelance project I developed that allows customers to browse and order furniture online, as well as submit requests for custom-made pieces. The system includes an admin panel where the shop owner can manage orders, customer records, and generate detailed sales and inventory reports for efficient business management.',
+        link: 'https://github.com/GherwinPacheco/postrado'
     },
     {
         title: 'Custom QR Code Generator Web App', 
         tools: ['HTML5', 'CSS', 'Javascript', 'React', 'Tailwind'],
         images: ['qr_1.png', 'qr_2.png', 'qr_3.png'],
-        description: 'The Custom QR Code Generator Web App is a project I developed during my internship at the Municipality of General Mariano Alvarez, Cavite. It was developed to help the municipality by enabling easy generation of QR Code IDs for Informal Settler Families. The web app is versatile and can also be used to create QR Code IDs for other purposes, making it a flexible tool for various identification needs.'
+        description: 'The Custom QR Code Generator Web App is a project I developed during my internship at the Municipality of General Mariano Alvarez, Cavite. It was developed to help the municipality by enabling easy generation of QR Code IDs for Informal Settler Families. The web app is versatile and can also be used to create QR Code IDs for other purposes, making it a flexible tool for various identification needs.',
+        link: 'https://gherwinpacheco.github.io/qrcode-generator/'
     },
     
 ];
--- src/contents/projects.jsx
+++ src/contents/projects.jsx
@@ -23,52 +23,54 @@ const Projects = () => {
 
             {MyProjects.map((item, index) => {
                 return (
-                    <BehindEffect key={index}  className='bg-gray-300' distance={10}>
-                    <div className="flex flex-col lg:flex-row mb-6 lg:mb-8 bg-card text-card-foreground border shadow p-5">
-                        <div className="flex-[1]">
+                    <BehindEffect key={index}  className='bg-gray-300 cursor-pointer' distance={10}>
+                    <a href={item.link}>
+                        <div className="flex flex-col lg:flex-row mb-6 lg:mb-8 bg-card text-card-foreground border shadow p-5">
+                            <div className="flex-[1]">
 
-                            {/* Visible on large screen */}
-                            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                                {item.title}
-                            </h4>
+                                {/* Visible on large screen */}
+                                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                                    {item.title}
+                                </h4>
 
-                            
-                            <div className="flex mb-6">
-                                <span className="text-muted-foreground">Tools Used:&emsp;</span>
-                                {
-                                    item.tools.map((tool, index) => {
-                                        const isLast = (index + 1) == item.tools.length;
-                                        return `${tool}${!isLast ? ', ' : ''}`
-                                    })
-                                }
                                 
-                            </div>
+                                <div className="flex mb-6">
+                                    <span className="text-muted-foreground">Tools Used:&emsp;</span>
+                                    {
+                                        item.tools.map((tool, index) => {
+                                            const isLast = (index + 1) == item.tools.length;
+                                            return `${tool}${!isLast ? ', ' : ''}`
+                                        })
+                                    }
+                                    
+                                </div>
+                                
+
+                                <p className="indent-10 text-justify hidden lg:block">
+                                    {item.description}
+                                </p>
                             
+                            </div>
+                            <div className="flex-[1] mb-6 lg:mb-0 lg:ml-20">
+                                <Carousel className='mb-4'>
+                                <CarouselContent>
+                                    {item.images.map((img, index) => {
+                                        const imgSource = new URL(`../assets/projects/${img}`, import.meta.url).href;
+                                        return (
+                                            <CarouselItem key={index} className='border-2'>
+                                                <img src={imgSource}/>
+                                            </CarouselItem>
+                                        )
+                                    })}
+                                </CarouselContent>
+                                </Carousel>
 
-                            <p className="indent-10 text-justify hidden lg:block">
-                                {item.description}
-                            </p>
-                        
+                                <p className="indent-10 text-justify lg:hidden">
+                                    {item.description}
+                                </p>
+                            </div>
                         </div>
-                        <div className="flex-[1] mb-6 lg:mb-0 lg:ml-20">
-                            <Carousel className='mb-4'>
-                            <CarouselContent>
-                                {item.images.map((img, index) => {
-                                    const imgSource = new URL(`../assets/projects/${img}`, import.meta.url).href;
-                                    return (
-                                        <CarouselItem key={index} className='border-2'>
-                                            <img src={imgSource}/>
-                                        </CarouselItem>
-                                    )
-                                })}
-                            </CarouselContent>
-                            </Carousel>
-
-                            <p className="indent-10 text-justify lg:hidden">
-                                {item.description}
-                            </p>
-                        </div>
-                    </div>
+                    </a>
                     </BehindEffect>
                 );
             })}
